df939d64c0fc1baf912a74865da5f0ee5cfc8c9e
tanisal <tanisal@gmail.com>
2022-05-31 09:44:53 +0300

    models and data for other cities are added

--- model/data_scraping.py
+++ model/data_scraping.py
@@ -10,14 +10,14 @@ driver = webdriver.Chrome(chrome_driver)
 #Function to extract the links of all the ads for the given number of pages
 def all_pages_list_of_ads(pages):
     #WE go to the first page
-    driver.get(r'https://www.imot.bg/pcgi/imot.cgi?act=3&slink=81fllq&f1=1')
+    driver.get(r'https://www.imot.bg/pcgi/imot.cgi?act=3&slink=82sgje&f1=1')
     #Click on the go on page pop menu
     driver.find_element_by_xpath('/html/body/div[5]/div[2]/div[1]/div[2]/div[2]/button[1]/p').click()
     #Create an empty list to store all the links from the loop
     all_links=[]
     #traverse through all the links n a page, append the links in a list and then go on a another page
     for page in range(1,pages+1):
-        url =r'https://www.imot.bg/pcgi/imot.cgi?act=3&slink=81fllq&f1={}'.format(page)
+        url =r'https://www.imot.bg/pcgi/imot.cgi?act=3&slink=82sgje&f1={}'.format(page)
         driver.get(url)
         #Creating a soup object with the page source
         soup = BeautifulSoup(driver.page_source, 'html.parser')
@@ -112,5 +112,6 @@ df =pd.DataFrame(get_house_data(app_links))
 df.columns=['location','price','m2','price/m2','details','floor','build']
 
 
-df.to_csv(r"imotibg.csv",index=False)
+df.to_csv(r"imotibg_Varna.csv",index=False)
 print("Created CSV File")
+
